44e4ac45554ac35adfc97d94db6a4313aa02ae67
Leila Inagas <leiladip@MacBook-Pro-de-Leila.local>
2022-03-03 22:20:30 -0300

    fix file 14

--- src/bases/14-ternary.js
+++ src/bases/14-ternary.js
@@ -1,1 +1,12 @@
 console.log('index')
+
+let firstName;
+let lastName = 'Inagas';
+
+console.log(`${firstName || 'No firstName'} ${lastName || 'No lastName'}`);
+
+const isActive = true
+
+const message = isActive ? 'yes' : 'no'
+
+console.log(message);
